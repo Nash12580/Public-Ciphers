f7d498d95bef1cb81ad7c66ba2ae4814213ad574
Nash12580 <nashalivicente340@gmail.com>
2025-02-04 14:06:23 -0800

    Update Diffie-Hellman.py

--- Diffie-Hellman.py
+++ Diffie-Hellman.py
@@ -1,3 +1,20 @@
+import random
+import hashlib 
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+import os
+
+# Params
+q = int("".join([
+        "B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C6",
+        "9A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C0",
+        "13ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD70",
+        "98488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0",
+        "A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708",
+        "DF1FB2BC2E4A4371"
+    ]), 16)
+
+
 alpha = int("".join([
         "A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507F",
         "D6406CFF14266D31266FEA1E5C41564B777E690F5504F213",
@@ -38,3 +55,4 @@ m1_decrypted = unpad(decipher.decrypt(c1), AES.block_size)
 print(f"Alice's Encrypted Message: {c0.hex()}")
 print(f"Alice's Decrypted Message: {m1_decrypted.decode()}")
 print(f"Bob's Encrypted Message: {c1.hex()}")
+print(f"Bob's Decrypted Message: {m0_decrypted.decode()}")
