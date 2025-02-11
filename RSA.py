fad2014dd3c9fcfd2e656cc6fcd0e307be5f9eba
hannah <113932134+hanmosh@users.noreply.github.com>
2025-02-11 11:43:07 -0800

    MITM RSA

--- RSA.py
+++ RSA.py
@@ -1,4 +1,5 @@
 from Crypto.Util.number import getPrime, GCD
+from Crypto.Random import get_random_bytes
 
 def multiplicative_inverse(e, phi):
     d = 0
@@ -48,9 +49,39 @@ def decrypt(prk, ciphertext):
     return plain.to_bytes(numbytes, 'big').decode('utf-8')
 
 if __name__ == '__main__':
+    
+    #### Reg RSA - add 'decode('utf-8')' to decrypt function ####
+
+    # public, private = generate_keypair(1024)
+    # message = "hello world!"
+    # encrypted_msg = encrypt(public, message)
+    # print("Encrypted:", encrypted_msg)
+    # decrypted_msg = decrypt(private, encrypted_msg)
+    # print("Decrypted:", decrypted_msg)
+
+
+    #### MITM - remove 'decode('utf-8')' from decrypt function ####
+    
     public, private = generate_keypair(1024)
-    message = "hello world!"
-    encrypted_msg = encrypt(public, message)
-    print("Encrypted:", encrypted_msg)
-    decrypted_msg = decrypt(private, encrypted_msg)
-    print("Decrypted:", decrypted_msg)
+
+    # Alice's secret message 
+    s = "Hello Bob!"  
+    encrypted_s = encrypt(public, s)
+    print("Original s:", s)
+
+    # Mallory intercepts and manipulates
+    e, n = public
+    r = int.from_bytes(get_random_bytes(64), 'big') % n
+    r_inv = multiplicative_inverse(r, n) 
+
+    c_prime = (encrypted_s * pow(r, e, n)) % n  
+
+    # Alice decrypts c' (m' = m * r mod n)
+    decrypted_bytes = decrypt(private, c_prime)
+    
+    # Mallory computes original message m
+    m = int.from_bytes(decrypted_bytes, 'big') * r_inv % n
+    original_m_bytes = m.to_bytes((m.bit_length() + 7) // 8, 'big')
+
+    original_m = original_m_bytes.decode('utf-8')
+    print("Recovered s by Mallory:", original_m)
